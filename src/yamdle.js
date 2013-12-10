0be8a7d7649b16ebbfde1149634d73e343619ffb
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2013-12-10 17:37:16 -0600

    Add yamdle.parse, implement scalar parsing
    - rename isCharType() to isType() and change signature to accept a
      string and index instead of a 1-length string
    - isType() now returns the last index of the matched string
    - add convert() to convert scalar strings into Javascript types

--- src/yamdle.js
+++ src/yamdle.js
@@ -105,7 +105,7 @@ define(function() {
     }
 
     var plain = false;
-    if (yamdle.isCharType(str[0], 'ns-plain-first', context)) {
+    if (yamdle.isType(str, 0, 'ns-plain-first', context) !== false) {
       plain = true;
       for (var i = 1, ilen = str.length; i < ilen; i++) {
         var ch = str[i];
@@ -122,7 +122,7 @@ define(function() {
           break;
         }
 
-        if (!yamdle.isCharType(ch, 'ns-plain-safe', context)) {
+        if (yamdle.isType(str, i, 'ns-plain-safe', context) === false) {
           // not safe for plain
           plain = false;
           break;
@@ -142,73 +142,383 @@ define(function() {
     return plain;
   };
 
-  yamdle.isCharType = function(ch, type, context) {
+  yamdle.isType = function(str, i, type, context) {
     // readable, but slow
-    var c = ch.charCodeAt(0);
+    if (i >= str.length) {
+      return false;
+    }
+
+    var ch = str[i];
+    var c = str.charCodeAt(i);
+    if (typeof(context) == 'undefined') {
+      context = 'flow-out';
+    }
+
     switch (type) {
       case 'c-printable':
-        return(c == 0x9 || c == 0xA || c == 0xD || (c >= 0x20 && c <= 0x7E) ||
-               c == 0x85 || (c >= 0xA0 && c <= 0xD7FF) ||
-               (c >= 0xE000 && c <= 0xFFFD));
+        if ((c == 0x9 || c == 0xA || c == 0xD || (c >= 0x20 && c <= 0x7E) ||
+             c == 0x85 || (c >= 0xA0 && c <= 0xD7FF) ||
+            (c >= 0xE000 && c <= 0xFFFD))) {
+          return i;
+        }
+        return false;
 
       case 'c-indicator':
-        return(ch == '-' || ch == '?' || ch == ':' || ch == ',' || ch == '[' ||
-               ch == ']' || ch == '{' || ch == '}' || ch == '#' || ch == '&' ||
-               ch == '*' || ch == '!' || ch == '|' || ch == '>' || ch == "'" ||
-               ch == '"' || ch == '%' || ch == '@' || ch == '`');
+        if (ch == '-' || ch == '?' || ch == ':' || ch == ',' || ch == '[' ||
+            ch == ']' || ch == '{' || ch == '}' || ch == '#' || ch == '&' ||
+            ch == '*' || ch == '!' || ch == '|' || ch == '>' || ch == "'" ||
+            ch == '"' || ch == '%' || ch == '@' || ch == '`') {
+          return i;
+        }
+        return false;
 
       case 'c-flow-indicator':
-        return(ch == ',' || ch == '[' || ch == ']' || ch == '{' || ch == '}');
+        if (ch == ',' || ch == '[' || ch == ']' || ch == '{' || ch == '}') {
+          return i;
+        }
+        return false;
 
       case 'b-char':
-        return(c == 0xA || c == 0xD);
+        if (c == 0xA || c == 0xD) {
+          return i;
+        };
+        return false;
 
       case 'c-byte-order-mark':
-        return(c == 0xFEFF);
+        if (c == 0xFEFF) {
+          return i;
+        }
+        return false;
 
       case 'nb-char':
-        return(yamdle.isCharType(ch, 'c-printable') &&
-               !yamdle.isCharType(ch, 'b-char') &&
-               !yamdle.isCharType(ch, 'c-byte-order-mark'));
+        if (yamdle.isType(str, i, 'c-printable', context) !== false &&
+            yamdle.isType(str, i, 'b-char', context) === false &&
+            yamdle.isType(str, i, 'c-byte-order-mark', context) === false) {
+          return i;
+        }
+        return false;
 
       case 's-white':
-        return(c == 0x20 || c == 0x9);
+        if (c == 0x20 || c == 0x9) {
+          return i;
+        }
+        return false;
 
       case 'ns-char':
-        return(yamdle.isCharType(ch, 'nb-char') &&
-               !yamdle.isCharType(ch, 's-white'));
+        if (yamdle.isType(str, i, 'nb-char', context) !== false &&
+            yamdle.isType(str, i, 's-white', context) === false) {
+          return i;
+        }
+        return false;
 
       case 'ns-plain-first':
-        return(yamdle.isCharType(ch, 'ns-char') &&
-               (ch == '?' || ch == ':' || ch == '-' ||
-                !yamdle.isCharType(ch, 'c-indicator')));
+        if (yamdle.isType(str, i, 'ns-char', context) !== false &&
+            (ch == '?' || ch == ':' || ch == '-' ||
+             yamdle.isType(str, i, 'c-indicator', context) === false)) {
+          return i;
+        }
+        return false;
 
       case 'ns-plain-safe':
-        if (typeof(context) == 'undefined') {
-          context = 'flow-out';
-        }
-
         switch (context) {
           case 'flow-out':
           case 'block-key':
-            return(yamdle.isCharType(ch, 'ns-plain-safe-out'));
+            return yamdle.isType(str, i, 'ns-plain-safe-out', context);
+
           case 'flow-in':
           case 'flow-key':
-            return(yamdle.isCharType(ch, 'ns-plain-safe-in'));
+            return yamdle.isType(str, i, 'ns-plain-safe-in', context);
 
           default:
             throw "invalid context";
         }
 
       case 'ns-plain-safe-out':
-        return(yamdle.isCharType(ch, 'ns-char'));
+        return yamdle.isType(str, i, 'ns-char', context);
 
       case 'ns-plain-safe-in':
-        return(yamdle.isCharType(ch, 'ns-char') &&
-               !yamdle.isCharType(ch, 'c-flow-indicator'));
+        if (yamdle.isType(str, i, 'ns-char', context) !== false &&
+            yamdle.isType(str, i, 'c-flow-indicator', context) === false) {
+          return i;
+        }
+        return false;
+
+      case 'ns-plain-char':
+        if (ch != ':' && ch != '#' && yamdle.isType(str, i, 'ns-plain-safe', context)) {
+          return i;
+        }
+        if (yamdle.isType(str, i, 'ns-char', context) !== false && i < (str.length - 1) && str[i+1] == '#') {
+          return i+1;
+        }
+        if (ch == ':' && yamdle.isType(str, i+1, 'ns-plain-safe', context)) {
+          return i+1;
+        }
+        return false;
+
+      case 'nb-ns-plain-in-line':
+        var j = i;
+        while (j < str.length) {
+          var result = yamdle.isType(str, j, 's-white', context);
+          while (result !== false) {
+            j = result;
+            result = yamdle.isType(str, j+1, 's-white', context);
+          }
+
+          var result = yamdle.isType(str, j, 'ns-plain-char', context);
+          if (result === false) {
+            break;
+          }
+          while (result !== false) {
+            j = result;
+            result = yamdle.isType(str, j+1, 'ns-plain-char', context);
+          }
+          i = j;
+          j++;
+        }
+        return i;
+
+      case 'ns-plain-multi-line':
+        // not currently supported
+        return yamdle.isType(str, i, 'ns-plain-one-line', context);
+
+      case 'ns-plain-one-line':
+        if (yamdle.isType(str, i, 'ns-plain-first', context) === false) {
+          return false;
+        }
+        return yamdle.isType(str, i+1, 'nb-ns-plain-in-line', context);
+
+      case 'ns-plain':
+        switch (context) {
+          case 'flow-out':
+          case 'flow-in':
+            return yamdle.isType(str, i, 'ns-plain-multi-line', context);
+
+          case 'block-key':
+          case 'flow-key':
+            return yamdle.isType(str, i, 'ns-plain-one-line', context);
+        }
+
+      case 'ns-dec-digit':
+        if (c >= 0x30 && c <= 0x39) {
+          return i;
+        }
+        return false;
+
+      case 'ns-hex-digit':
+        if (yamdle.isType(str, i, 'ns-dec-digit', context) !== false ||
+            (c >= 0x41 && c <= 0x46) || (c >= 0x61 && c <= 0x66)) {
+          return i;
+        }
+        return false;
+
+      case 'c-ns-esc-char':
+        if (ch != '\\' || i == (str.length - 1)) {
+          return false;
+        }
+
+        i++;
+        ch = str[i];
+        c = str.charCodeAt(i);
+        if (ch == '0' || ch == 'a' || ch == 'b' || ch == 't' || c == 0x9 ||
+            ch == 'n' || ch == 'v' || ch == 'f' || ch == 'r' || ch == 'e' ||
+            c == 0x20 || ch == '"' || ch == '/' || ch == '\\' || ch == 'N' ||
+            ch == '_' || ch == 'L' || ch == 'P') {
+          return i;
+        }
+
+        // unicode
+        var num;
+        if (ch == 'x') {
+          num = 2;
+        }
+        else if (ch == 'u') {
+          num = 4;
+        }
+        else if (ch == 'U') {
+          num = 8;
+        }
+        else {
+          return false;
+        }
+
+        i++;
+        for (var j = 0; j < num; i++, j++) {
+          if (yamdle.isType(str, i, 'ns-hex-digit', context) === false) {
+            return false;
+          }
+        }
+        return i;
+
+      case 'nb-json':
+        if (c == 0x9 || (c >= 0x20 && c <= 0x10FFFF)) {
+          return i;
+        }
+        return false;
+
+      case 'nb-double-char':
+        var result = yamdle.isType(str, i, 'c-ns-esc-char', context);
+        if (result !== false) {
+          return result;
+        }
+
+        if (ch != '\\' && ch != '"' && yamdle.isType(str, i, 'nb-json', context) !== false) {
+          return i;
+        }
+        return false;
+
+      case 'nb-double-one-line':
+        var result = yamdle.isType(str, i, 'nb-double-char', context);
+        while (result !== false) {
+          i = result;
+          if (i >= (str.length - 1)) {
+            break;
+          }
+          result = yamdle.isType(str, i+1, 'nb-double-char', context);
+        }
+        return i;
+
+      case 'nb-double-multi-line':
+        // not currently supported
+        return yamdle.isType(str, i, 'nb-double-one-line', context);
+
+      case 'c-double-quoted':
+        if (ch != '"') {
+          return false;
+        }
+        i++;
+
+        var nextType;
+        switch (context) {
+          case 'flow-out':
+          case 'flow-in':
+            nextType = 'nb-double-multi-line';
+            break;
+
+          case 'block-key':
+          case 'flow-key':
+            nextType = 'nb-double-one-line';
+            break;
+        }
+
+        i = yamdle.isType(str, i, nextType, context);
+        if (i === false) {
+          return false;
+        }
+
+        i++;
+        if (str[i] == '"') {
+          return i;
+        }
+        return false;
 
       default:
         throw "invalid character type";
+    }
+  };
+
+  yamdle.convert = function(type, str) {
+    switch (type) {
+      case 'ns-plain':
+        if (str.match(/^(0|-?[1-9][0-9]*)$/)) {
+          return parseInt(str);
+        }
+        if (str.match(/^-?[1-9][0-9]*(\.[0-9]*)?(e[-+][0-9]+)?$/)) {
+          return parseFloat(str);
+        }
+        if (str == "true") {
+          return true;
+        }
+        if (str == "false") {
+          return false;
+        }
+        if (str == "null") {
+          return null;
+        }
+        return str;
+
+      case 'c-double-quoted':
+        var value = '';
+        for (var i = 1; i < (str.length - 1); i++) {
+          if (str[i] == '\\') {
+            i++;
+            var ch = str[i];
+            var c = str.charCodeAt(i);
+            if (ch == '0') {
+              value += '\0';
+            } else if (ch == 'a') {
+              value += '\u0007';
+            } else if (ch == 'b') {
+              value += '\b';
+            } else if (ch == 't' || c == 9) {
+              value += '\t';
+            } else if (ch == 'n') {
+              value += '\n';
+            } else if (ch == 'v') {
+              value += '\u000b';
+            } else if (ch == 'f') {
+              value += '\f';
+            } else if (ch == 'r') {
+              value += '\r';
+            } else if (ch == 'e') {
+              value += '\u001b';
+            } else if (c == 20) {
+              value += ' ';
+            } else if (ch == '"') {
+              value += '"'
+            } else if (ch == '/') {
+              value += '/';
+            } else if (ch == '\\') {
+              value += '\\';
+            } else if (ch == 'N') {
+              value += '\u0085';
+            } else if (ch == '_') {
+              value += '\u00a0';
+            } else if (ch == 'L') {
+              value += '\u2028';
+            } else if (ch == 'P') {
+              value += '\u2029';
+            } else if (ch == 'x') {
+              value += String.fromCharCode(parseInt(str.substr(i+1, 2), 16));
+              i += 2
+            } else if (ch == 'u') {
+              value += String.fromCharCode(parseInt(str.substr(i+1, 4), 16));
+              i += 4
+            } else if (ch == 'U') {
+              // unsupported
+            }
+          }
+          else {
+            value += str[i];
+          }
+        }
+        return value;
+    }
+  };
+
+  yamdle.parse = function(str) {
+    var context;
+
+    var i = 0;
+    var events = [];
+    while (i < (str.length - 1)) {
+      var start = i, end;
+      var type;
+      if ((end = yamdle.isType(str, i, 'ns-plain', context)) !== false) {
+        type = 'ns-plain';
+      }
+      else if ((end = yamdle.isType(str, i, 'c-double-quoted', context)) !== false) {
+        type = 'c-double-quoted';
+      }
+      else {
+        break;
+      }
+
+      events.push({type: type, value: yamdle.convert(type, str.substring(start, end + 1))});
+      i = end + 1;
+    }
+
+    if (events.length > 0) {
+      return events[events.length - 1].value;
     }
   };
 
--- test/js/test_yamdle.js
+++ test/js/test_yamdle.js
@@ -3,96 +3,196 @@ define([
   'yamdle'
 ], function(prod, yamdle) {
   return new prod.Suite('yamdle', {
-    'plain string': function() {
-      this.assertEquals("foo", yamdle.stringify("foo"));
-    },
+    'stringify': new prod.Suite('stringify', {
+      'plain string': function() {
+        this.assertEquals("foo", yamdle.stringify("foo"));
+      },
 
-    'non-plain string with quote': function() {
-      this.assertEquals('"!f\\"oo"', yamdle.stringify('!f"oo'));
-    },
+      'non-plain string with quote': function() {
+        this.assertEquals('"!f\\"oo"', yamdle.stringify('!f"oo'));
+      },
 
-    'non-plain string with blackslash': function() {
-      this.assertEquals('"!f\\\\oo"', yamdle.stringify('!f\\oo'));
-    },
+      'non-plain string with blackslash': function() {
+        this.assertEquals('"!f\\\\oo"', yamdle.stringify('!f\\oo'));
+      },
 
-    'number string': function() {
-      this.assertEquals('"123"', yamdle.stringify("123"));
-    },
+      'number string': function() {
+        this.assertEquals('"123"', yamdle.stringify("123"));
+      },
 
-    'float string': function() {
-      this.assertEquals('"123.4"', yamdle.stringify("123.4"));
-    },
+      'float string': function() {
+        this.assertEquals('"123.4"', yamdle.stringify("123.4"));
+      },
 
-    'boolean string': function() {
-      this.assertEquals('"true"', yamdle.stringify("true"));
-      this.assertEquals('"false"', yamdle.stringify("false"));
-    },
+      'boolean string': function() {
+        this.assertEquals('"true"', yamdle.stringify("true"));
+        this.assertEquals('"false"', yamdle.stringify("false"));
+      },
 
-    'float string': function() {
-      this.assertEquals('"null"', yamdle.stringify("null"));
-    },
+      'null string': function() {
+        this.assertEquals('"null"', yamdle.stringify("null"));
+      },
 
-    'sequence string': function() {
-      this.assertEquals('"[]"', yamdle.stringify("[]"));
-    },
+      'sequence string': function() {
+        this.assertEquals('"[]"', yamdle.stringify("[]"));
+      },
 
-    'map string': function() {
-      this.assertEquals('"{}"', yamdle.stringify("{}"));
-    },
+      'map string': function() {
+        this.assertEquals('"{}"', yamdle.stringify("{}"));
+      },
 
-    'integer': function() {
-      this.assertEquals("123", yamdle.stringify(123));
-    },
+      'integer': function() {
+        this.assertEquals("123", yamdle.stringify(123));
+      },
 
-    'float': function() {
-      this.assertEquals("123.4", yamdle.stringify(123.4));
-    },
+      'float': function() {
+        this.assertEquals("123.4", yamdle.stringify(123.4));
+      },
 
-    'boolean': function() {
-      this.assertEquals("true", yamdle.stringify(true));
-      this.assertEquals("false", yamdle.stringify(false));
-    },
+      'boolean': function() {
+        this.assertEquals("true", yamdle.stringify(true));
+        this.assertEquals("false", yamdle.stringify(false));
+      },
 
-    'null': function() {
-      this.assertEquals("null", yamdle.stringify(null));
-    },
+      'null': function() {
+        this.assertEquals("null", yamdle.stringify(null));
+      },
 
-    'array': function() {
-      this.assertEquals("- foo\n- bar", yamdle.stringify(['foo', 'bar']));
-    },
+      'array': function() {
+        this.assertEquals("- foo\n- bar", yamdle.stringify(['foo', 'bar']));
+      },
 
-    'empty array': function() {
-      this.assertEquals("[]", yamdle.stringify([]));
-    },
+      'empty array': function() {
+        this.assertEquals("[]", yamdle.stringify([]));
+      },
 
-    'nested arrays': function() {
-      var arr = [['foo', 'bar'], 'baz'];
-      this.assertEquals("- - foo\n  - bar\n- baz", yamdle.stringify(arr));
-    },
+      'nested arrays': function() {
+        var arr = [['foo', 'bar'], 'baz'];
+        this.assertEquals("- - foo\n  - bar\n- baz", yamdle.stringify(arr));
+      },
 
-    'array with object': function() {
-      var arr = ['foo', {bar: 'baz', qux: 'corge'}];
-      this.assertEquals("- foo\n- bar: baz\n  qux: corge", yamdle.stringify(arr));
-    },
+      'array with object': function() {
+        var arr = ['foo', {bar: 'baz', qux: 'corge'}];
+        this.assertEquals("- foo\n- bar: baz\n  qux: corge", yamdle.stringify(arr));
+      },
 
-    'object': function() {
-      var obj = {foo: 'bar', baz: 'qux'};
-      this.assertEquals("foo: bar\nbaz: qux", yamdle.stringify(obj));
-    },
+      'object': function() {
+        var obj = {foo: 'bar', baz: 'qux'};
+        this.assertEquals("foo: bar\nbaz: qux", yamdle.stringify(obj));
+      },
 
-    'object with empty string value': function() {
-      var obj = {foo: ''};
-      this.assertEquals('foo: ""', yamdle.stringify(obj));
-    },
+      'object with empty string value': function() {
+        var obj = {foo: ''};
+        this.assertEquals('foo: ""', yamdle.stringify(obj));
+      },
 
-    'object with array value': function() {
-      var obj = {foo: 'bar', baz: ['qux', 'corge']};
-      this.assertEquals("foo: bar\nbaz:\n  - qux\n  - corge", yamdle.stringify(obj));
-    },
+      'object with array value': function() {
+        var obj = {foo: 'bar', baz: ['qux', 'corge']};
+        this.assertEquals("foo: bar\nbaz:\n  - qux\n  - corge", yamdle.stringify(obj));
+      },
 
-    'nested object values': function() {
-      var obj = {foo: 'bar', baz: {qux: 'corge', grault: 'garply'}};
-      this.assertEquals("foo: bar\nbaz:\n  qux: corge\n  grault: garply", yamdle.stringify(obj));
-    }
+      'nested object values': function() {
+        var obj = {foo: 'bar', baz: {qux: 'corge', grault: 'garply'}};
+        this.assertEquals("foo: bar\nbaz:\n  qux: corge\n  grault: garply", yamdle.stringify(obj));
+      }
+    }),
+
+    'parse': new prod.Suite('parse', {
+      'plain string': function() {
+        this.assertEquals("foo", yamdle.parse("foo"));
+      },
+
+      'non-plain string with quote': function() {
+        this.assertEquals('!f"oo', yamdle.parse('"!f\\"oo"'));
+      },
+
+      'non-plain string with blackslash': function() {
+        this.assertEquals('!f\\oo', yamdle.parse('"!f\\\\oo"'));
+      },
+
+      'number string': function() {
+        this.assertEquals("123", yamdle.parse('"123"'));
+      },
+
+      'float string': function() {
+        this.assertEquals("123.4", yamdle.parse('"123.4"'));
+      },
+
+      'boolean string': function() {
+        this.assertEquals("true", yamdle.parse('"true"'));
+        this.assertEquals("false", yamdle.parse('"false"'));
+      },
+
+      'null string': function() {
+        this.assertEquals("null", yamdle.parse('"null"'));
+      },
+
+      /*
+      'sequence string': function() {
+        this.assertEquals('"[]"', yamdle.stringify("[]"));
+      },
+
+      'map string': function() {
+        this.assertEquals('"{}"', yamdle.stringify("{}"));
+      },
+      */
+
+      'integer': function() {
+        this.assertEquals(123, yamdle.parse("123"));
+      },
+
+      'float': function() {
+        this.assertEquals(123.4, yamdle.parse("123.4"));
+      },
+
+      'boolean': function() {
+        this.assertEquals(true, yamdle.parse("true"));
+        this.assertEquals(false, yamdle.parse("false"));
+      },
+
+      'null': function() {
+        this.assertEquals(null, yamdle.parse("null"));
+      },
+
+      /*
+      'array': function() {
+        this.assertEquals("- foo\n- bar", yamdle.stringify(['foo', 'bar']));
+      },
+
+      'empty array': function() {
+        this.assertEquals("[]", yamdle.stringify([]));
+      },
+
+      'nested arrays': function() {
+        var arr = [['foo', 'bar'], 'baz'];
+        this.assertEquals("- - foo\n  - bar\n- baz", yamdle.stringify(arr));
+      },
+
+      'array with object': function() {
+        var arr = ['foo', {bar: 'baz', qux: 'corge'}];
+        this.assertEquals("- foo\n- bar: baz\n  qux: corge", yamdle.stringify(arr));
+      },
+
+      'object': function() {
+        var obj = {foo: 'bar', baz: 'qux'};
+        this.assertEquals("foo: bar\nbaz: qux", yamdle.stringify(obj));
+      },
+
+      'object with empty string value': function() {
+        var obj = {foo: ''};
+        this.assertEquals('foo: ""', yamdle.stringify(obj));
+      },
+
+      'object with array value': function() {
+        var obj = {foo: 'bar', baz: ['qux', 'corge']};
+        this.assertEquals("foo: bar\nbaz:\n  - qux\n  - corge", yamdle.stringify(obj));
+      },
+
+      'nested object values': function() {
+        var obj = {foo: 'bar', baz: {qux: 'corge', grault: 'garply'}};
+        this.assertEquals("foo: bar\nbaz:\n  qux: corge\n  grault: garply", yamdle.stringify(obj));
+      }
+      */
+    })
   });
 });
